8dfb66cfd4d7368c6821bfd3a7163a69960e22a0
patpietryg <patpietryg@gmail.com>
2024-03-31 10:00:56 +0200

    Added generate_password_hash function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 import hashlib
 import secrets
 import re
+import getpass
 
 
 def add_user():
@@ -11,17 +12,26 @@ def add_user():
             print("The login must be from 3 to 15 characters and contain only alphanumeric characters and at least one special character !@#$%^&*().")
             continue
 
-        password = input("Enter your password: ")
+        password = getpass.getpass(prompt='Enter your password: ')
         if not (8 <= len(password) <= 25 and re.search("[!@#$%^&*()]+", password)):
             print("The password must be between 8 and 25 characters long and contain at least one special character !@#$%^&*().")
             continue
 
-        confirm_password = input("Confirm password: ")
+        confirm_password = getpass.getpass(prompt='Confirm password: ')
         if password != confirm_password:
             print("The passwords provided are not identical.")
             continue
 
         break
 
-    #dodanie do bazy
+    hashed_password, salt = generate_password_hash(password)
 
+def generate_password_hash(password):
+    # Generowanie losowej soli o długości 16 bajtów
+    salt = secrets.token_bytes(16)
+
+    # Łączenie hasła z solą i haszowanie
+    salted_password = salt + password.encode('utf-8')
+    hashed_password = hashlib.sha256(salted_password).hexdigest()
+
+    return hashed_password, salt.hex()
